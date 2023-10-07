0295dd6bb2fbb3fc3ee63c355d3d5ffe7fc96866
Radmir <59477654+Therad445@users.noreply.github.com>
2023-10-07 11:15:18 +0300

    Update lab2.py

--- Lab1/Task5/lab2.py
+++ Lab1/Task5/lab2.py
@@ -1,1 +1,66 @@
+import os
+from datetime import datetime
+import json
+import sys
+
+# ... (ваш код существующих функций log_err, log_info, и других)
+
+def encode_file(input_file, output_file):
+    # Здесь реализуйте кодирование файла input_file и запись его в output_file
+    # Включите сигнатуру и коды алгоритмов в заголовок архива (согласно Л1.№4)
+    # Копируйте содержимое input_file в архив output_file без преобразований
+
+def decode_file(input_file, output_file):
+    # Здесь реализуйте декодирование архива input_file и запись его в output_file
+    # Проверьте сигнатуру архива, коды алгоритмов и выполните соответствующие действия
+
+def main():
+    if len(sys.argv) != 4:
+        print("Использование: python codec.py [--encode|--decode] input_file output_file")
+        return
+
+    action = sys.argv[1]
+    input_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    if action == "--encode":
+        encode_file(input_file, output_file)
+        print("Файл успешно закодирован.")
+    elif action == "--decode":
+        decode_file(input_file, output_file)
+        print("Файл успешно декодирован.")
+    else:
+        print("Неверная команда. Используйте --encode или --decode.")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+# ... (ваш код существующих функций log_err, log_info, и других)
+
+SIGNATURE = "MY_CODEC_SIGNATURE"
+
+def encode_file(input_file, output_file):
+    # Открываем выходной файл для записи
+    with open(output_file, "wb") as out_file:
+        # Записываем сигнатуру в архив
+        out_file.write(SIGNATURE.encode("utf-8"))
+        
+        # Здесь реализуйте кодирование файла input_file и запись его в output_file
+        # Копируйте содержимое input_file в архив output_file без преобразований
+
+def decode_file(input_file, output_file):
+    # Открываем входной файл для чтения
+    with open(input_file, "rb") as in_file:
+        # Читаем сигнатуру из архива
+        signature = in_file.read(len(SIGNATURE))
+
+        if signature != SIGNATURE.encode("utf-8"):
+            print("Неверная сигнатура. Файл не может быть декодирован.")
+            return
+
+        # Здесь реализуйте декодирование архива input_file и запись его в output_file
+        # Проверьте коды алгоритмов и выполните соответствующие действия
 

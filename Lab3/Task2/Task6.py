1dd9b3c86f7d6baaa16aa3932fd77a2b38095024
Radmir <59477654+Therad445@users.noreply.github.com>
2023-10-07 11:38:03 +0300

    Update Task6.py

--- Lab3/Task2/Task6.py
+++ Lab3/Task2/Task6.py
@@ -1,1 +1,13 @@
+def calculate_huffman_codes_and_lengths(data, bit_depth):
+    # Ваш код для вычисления кодов Хаффмана
+    pass
+
+def calculate_lengths(data, bit_depth):
+    # Ваш код для вычисления Ci и Di
+    pass
+
+# Пример использования:
+file_data = read_file("example_file.bin")  # Замените на чтение данных из файла
+bit_depth = 8  # Замените на нужную разрядность (8, 4 или 32)
+calculate_huffman_codes_and_lengths(file_data, bit_depth)
 

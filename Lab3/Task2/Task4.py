212278b39e347b947671c96319a2b4583419ada4
Radmir <59477654+Therad445@users.noreply.github.com>
2023-10-07 11:37:35 +0300

    Update Task4.py

--- Lab3/Task2/Task4.py
+++ Lab3/Task2/Task4.py
@@ -1,1 +1,45 @@
+import pickle
+
+class IntervalNode:
+    def __init__(self, low, high, char=None):
+        self.low = low
+        self.high = high
+        self.char = char
+
+def build_interval_tree(freq_dict):
+    # Реализуйте построение интервального дерева на основе частот символов
+    pass
+
+def encode_interval(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        data = file.read()
+
+    freq_dict = collections.Counter(data)
+    interval_tree = build_interval_tree(freq_dict)
+
+    # Ваш код для кодирования данных с использованием интервального алгоритма
+
+def decode_interval(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        # Ваш код для декодирования данных с использованием интервального алгоритма
+
+def decode_format(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        file_signature = file.read(len(signature))  # Замените на сигнатуру вашего формата
+        file_version = int.from_bytes(file.read(1), byteorder='big')
+        file_algorithm_code = int.from_bytes(file.read(1), byteorder='big')
+
+        if file_signature != signature or file_version != version:
+            print("Ошибка: Несовпадение сигнатуры или версии")
+            return
+
+        if file_algorithm_code == algorithm_code:
+            decode_interval(input_file, output_file)
+        else:
+            print(f"Ошибка: Неподдерживаемый код алгоритма: {file_algorithm_code}")
+
+# Пример использования:
+input_file = 'compressed.interval'  # Замените на путь к вашему сжатому файлу
+output_file = 'decompressed.txt'  # Замените на имя файла для разжатых данных
+decode_format(input_file, output_file)
 

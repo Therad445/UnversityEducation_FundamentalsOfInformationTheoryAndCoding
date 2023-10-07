75219ec3871898ffb98970c88e73222c3aec9d90
Radmir <59477654+Therad445@users.noreply.github.com>
2023-10-07 11:37:48 +0300

    Update Task5.py

--- Lab3/Task2/Task5.py
+++ Lab3/Task2/Task5.py
@@ -1,1 +1,46 @@
+import collections
+import pickle
+
+class ShannonFanoNode:
+    def __init__(self, char=None):
+        self.char = char
+        self.left = None
+        self.right = None
+
+def build_shannon_fano_tree(freq_dict):
+    # Реализуйте построение дерева Шеннона-Фано на основе частот символов
+    pass
+
+def encode_shannon_fano(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        data = file.read()
+
+    freq_dict = collections.Counter(data)
+    shannon_fano_tree = build_shannon_fano_tree(freq_dict)
+
+    # Ваш код для кодирования данных с использованием дерева Шеннона-Фано
+
+def decode_shannon_fano(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        # Ваш код для декодирования данных с использованием дерева Шеннона-Фано
+
+def decode_format(input_file, output_file):
+    with open(input_file, 'rb') as file:
+        file_signature = file.read(len(signature))  # Замените на сигнатуру вашего формата
+        file_version = int.from_bytes(file.read(1), byteorder='big')
+        file_algorithm_code = int.from_bytes(file.read(1), byteorder='big')
+        compressed_data = file.read()
+
+    if file_signature == signature and file_version == version:
+        if file_algorithm_code == algorithm_code:
+            decode_shannon_fano(input_file, output_file)
+        else:
+            print(f"Ошибка: Неподдерживаемый код алгоритма: {file_algorithm_code}")
+    else:
+        print("Ошибка: Несовпадение сигнатуры или версии")
+
+# Пример использования:
+input_file = 'compressed.shannon_fano'  # Замените на путь к вашему сжатому файлу
+output_file = 'decompressed.txt'  # Замените на имя файла для разжатых данных
+decode_format(input_file, output_file)
 
